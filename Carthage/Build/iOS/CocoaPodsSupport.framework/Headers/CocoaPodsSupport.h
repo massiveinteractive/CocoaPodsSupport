150593ab71673bfe95e8dcbb56f6b11beb233556
Eduardo Miranda <eduardo.miranda@massive.co>
2018-09-21 16:54:53 +0200

    changed PRODUCT_BUNDLE_IDENTIFIER = "$(PRODUCT_BUNDLE_IDENTIFIER)" added carthage build files

---
+++ Carthage/Build/iOS/CocoaPodsSupport.framework/Headers/CocoaPodsSupport.h
@@ -0,0 +1,17 @@
+//
+//  CocoaPodsSupport.h
+//  CocoaPodsSupport
+//
+//  Created by Pascal Simplice on 03/09/2018.
+//  Copyright © 2018 Massive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CocoaPodsSupport.
+FOUNDATION_EXPORT double CocoaPodsSupportVersionNumber;
+
+//! Project version string for CocoaPodsSupport.
+FOUNDATION_EXPORT const unsigned char CocoaPodsSupportVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaPodsSupport/PublicHeader.h>
